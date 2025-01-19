8f1fd36c81ef7bde5048e66e6fd088a87aec1b2a
TeeJarr <98375641+TeeJarr@users.noreply.github.com>
2025-01-18 23:28:01 -0700

    more setting changes and plugins added

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,38 +23,38 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 -- tree sitter
-require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
+require("lazy").setup({ { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" } })
 -- auto close
 require("autoclose").setup({
-   keys = {
+	keys = {
 
-      ["("] = { escape = true, close = true, pair = "()", disabled_filetypes = {} },
-   },
+		["("] = { escape = true, close = true, pair = "()", disabled_filetypes = {} },
+	},
 })
 -- mason
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 -- bufferline
 vim.opt.termguicolors = true
-require("bufferline").setup{
+require("bufferline").setup({
 	options = {
 		mode = "buffers",
 		offsets = {
@@ -62,28 +62,25 @@ require("bufferline").setup{
 				filetype = "NvimTree",
 				text = "File Explorer",
 				highlight = "Directory",
-				separator = "true"
-			}
+				separator = "true",
+			},
 		},
 	},
-}
--- auto tag
-require('nvim-ts-autotag').setup({
-  opts = {
-    -- Defaults
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = false -- Auto close on trailing </
-  },
-  -- Also override individual filetype configs, these take priority.
-  -- Empty by default, useful if one of the "opts" global settings
-  -- doesn't work well in a specific filetype
-  per_filetype = {
-    ["html"] = {
-      enable_close = false
-    }
-  }
 })
-
--- prettier 
-require('config.prettier-config')
+-- auto tag
+require("nvim-ts-autotag").setup({
+	opts = {
+		-- Defaults
+		enable_close = true, -- Auto close tags
+		enable_rename = true, -- Auto rename pairs of tags
+		enable_close_on_slash = false, -- Auto close on trailing </
+	},
+	-- Also override individual filetype configs, these take priority.
+	-- Empty by default, useful if one of the "opts" global settings
+	-- doesn't work well in a specific filetype
+	per_filetype = {
+		["html"] = {
+			enable_close = false,
+		},
+	},
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,12 @@
 local key = vim.keymap
 --Nvim Keymaps
-key.set('n', ';', ':')
+key.set("n", ";", ":")
 --Nvim-Tree Keymaps
-key.set('n', '<leader>ee', ':NvimTreeToggle<CR>')
-key.set('n', '<leader>ef', ':NvimTreeFocus<CR>')
+key.set("n", "<leader>ee", ":NvimTreeToggle<CR>")
+key.set("n", "<leader>ef", ":NvimTreeFocus<CR>")
 --Bufferline
+
+-- telescope
+key.set("n", "<leader>ft", ":Telescope<CR>")
+key.set("n", "<leader>ff", ":Telescope find_files<CR>")
+key.set("n", "<leader>fb", ":Telescope current_buffer_fuzzy_find<CR>")
